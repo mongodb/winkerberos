0ad630b2812c4dbdd10d063d31366584cbd7db71
Steven <steve.silvester@mongodb.com>
2024-05-01 15:13:39 -0500

    consistent use of braces

--- bump-and-tag.sh
+++ bump-and-tag.sh
@@ -9,16 +9,16 @@ echo "Running bump, tag, and bump..."
 set -x
 
 # Bump
-perl -pi -e "s/version = \"$CURRENT_VERSION\"/version = \"$NEW_VERSION\"/" pyproject.toml
+perl -pi -e "s/version = \"${CURRENT_VERSION}\"/version = \"${NEW_VERSION}\"/" pyproject.toml
 git add .
 git commit -a -m "BUMP $$NEW_VERSION}" -s --gpg-sign=${GPG_KEY_ID}
 
 # Tag
 git tag -a "$$NEW_VERSION}" -m "BUMP $$NEW_VERSION}" -s --local-user=${GPG_KEY_ID}
-git show --no-patch "$$NEW_VERSION}"
+git show --no-patch "${NEW_VERSION}"
 
 # Bump
-perl -pi -e "s/version = \"$NEW_VERSION\"/version = \"$POST_VERSION\"/" pyproject.toml
+perl -pi -e "s/version = \"${NEW_VERSION}\"/version = \"${POST_VERSION}\"/" pyproject.toml
 git add .
 git commit -a -m "BUMP ${POST_VERSION}" -s --gpg-sign=${GPG_KEY_ID}
 
