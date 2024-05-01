325fa9cc4f83ff9141720bcaf4078607fa990178
Steven <steve.silvester@mongodb.com>
2024-05-01 15:54:59 -0500

    fixups

--- bump-and-tag.sh
+++ bump-and-tag.sh
@@ -11,10 +11,10 @@ set -x
 # Bump
 perl -pi -e "s/version = \"${CURRENT_VERSION}\"/version = \"${NEW_VERSION}\"/" pyproject.toml
 git add .
-git commit -a -m "BUMP $$NEW_VERSION}" -s --gpg-sign=${GPG_KEY_ID}
+git commit -a -m "BUMP ${NEW_VERSION}" -s --gpg-sign=${GPG_KEY_ID}
 
 # Tag
-git tag -a "$$NEW_VERSION}" -m "BUMP $$NEW_VERSION}" -s --local-user=${GPG_KEY_ID}
+git tag -a "$$NEW_VERSION}" -m "BUMP ${NEW_VERSION}" -s --local-user=${GPG_KEY_ID}
 git show --no-patch "${NEW_VERSION}"
 
 # Bump
