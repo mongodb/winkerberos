6cef6e05e362206b13a54ac322d0d6223b0868e0
Steven <steve.silvester@mongodb.com>
2024-05-11 20:44:36 -0500

    refactor actions

---
+++ .github/actions/publish/publish.sh
@@ -0,0 +1,24 @@
+
+#!/usr/bin/env bash
+set -e
+
+echo "Show public outputs"
+echo "$GITHUB_RUN_ID" > release_run_id.txt
+ls -ltr signatures/*
+cat papertrail.txt
+cat release_run_id.txt
+
+if [ "$DRY_RUN" == "false" ]; then
+    echo "Uploading Release Reports"
+    export AWS_BUCKET=$(cat $AWS_BUCKET_FILE)
+    export TARGET=s3://$AWS_BUCKET/${{ inputs.product_name }}/${VERSION}
+    aws s3 cp ./signatures $TARGET --recursive
+    aws s3 cp papertrail.txt $TARGET
+
+    echo "Creating draft release with attached files"
+    gh release create ${VERSION} --draft --verify-tag --title ${VERSION} --notes ""
+    gh release upload ${VERSION} signatures/*.sig
+    gh release upload ${VERSION} release_run_id.txt
+else
+    echo "Dry run, not uploading to s3 or creating GitHub Release"
+fi
