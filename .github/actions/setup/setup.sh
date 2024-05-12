9693a57847e70aa66e42ee6b54476b4b96e21ab5
Steven <steve.silvester@mongodb.com>
2024-05-12 08:09:02 -0500

    cleanup

--- .github/actions/setup/setup.sh
+++ .github/actions/setup/setup.sh
@@ -6,7 +6,6 @@ SECRETS_FILE=/tmp/secret-value.json
 echo "$(aws secretsmanager get-secret-value --secret-id ${AWS_SECRET_ID} --query SecretString --output text)" > $SECRETS_FILE
 # Ensure sensitive secrets are masked in logs.
 ARTIFACTORY_USER=$(cat $SECRETS_FILE | jq -r '."artifactory-username"')
-echo "::add-mask::$ARTIFACTORY_USER"
 ARTIFACTORY_PASSWORD=$(cat $SECRETS_FILE | jq -r '."artifactory-password"')
 echo "::add-mask::$ARTIFACTORY_PASSWORD"
 GRS_CONFIG_USER1_USERNAME=$(cat $SECRETS_FILE | jq -r '."garasign-username"')
@@ -27,24 +26,21 @@ echo $ARTIFACTORY_PASSWORD | podman login -u $ARTIFACTORY_USER --password-stdin 
 podman pull $ARTIFACTORY_REGISTRY/$ARTIFACTORY_IMAGE
 echo "::endgroup::"
 
-echo "::group::Set up envfile for artifactory image"
+echo "Set up envfile for artifactory image"
 GARASIGN_ENVFILE=/tmp/envfile
 cat << EOF > $GARASIGN_ENVFILE
 GRS_CONFIG_USER1_USERNAME=$GRS_CONFIG_USER1_USERNAME
 GRS_CONFIG_USER1_PASSWORD=$GRS_CONFIG_USER1_PASSWORD
 EOF
-echo "::endgroup::"
 
-echo "::group::Set up global variables"
+echo "Set up global variables"
 echo "AWS_BUCKET=$AWS_BUCKET" >> $GITHUB_ENV
 echo "GPG_KEY_ID=$GPG_KEY_ID" >> $GITHUB_ENV
 echo "GPG_PUBLIC_URL=$GPG_PUBLIC_URL" >> $GITHUB_ENV
 echo "GARASIGN_ENVFILE=$GARASIGN_ENVFILE" >> $GITHUB_ENV
 echo "ARTIFACTORY_IMAGE=$ARTIFACTORY_IMAGE" >> $GITHUB_ENV
 echo "ARTIFACTORY_REGISTRY=$ARTIFACTORY_REGISTRY" >> $GITHUB_ENV
-echo "::endgroup::"
 
-echo "::group::Set up git config"
+echo "Set up git config"
 git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config user.name "github-actions[bot]"
-echo "::endgroup::"
