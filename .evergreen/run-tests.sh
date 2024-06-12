58105d3e8ae6acfe41bbed9cd3712383fdda7a51
Steven <steve.silvester@mongodb.com>
2024-06-12 06:24:02 -0500

    fixup

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
 # Disable xtrace for security reasons (just in case it was accidentally set).
 set +x
+set -eu
 
 # Fetch secrets
 bash ${DRIVERS_TOOLS}/.evergreen/secrets_handling/setup-secrets.sh drivers/enterprise_auth
-source ${DRIVERS_TOOLS}/.evergreen/setup-secrets.sh
+source ${DRIVERS_TOOLS}/.evergreen/secrets_handling/secrets-export.sh
 
 # Map secrets
 export KERBEROS_SERVICE=${PRINCIPAL}
@@ -16,7 +17,7 @@ export KERBEROS_PASSWORD=
 
 # Set up env
 "C:/python/Python38/python.exe" -m venv .venv
-.venv/bin/activate
+.venv/Scripts/activate
 pip install pymongo pytest
 pip install -e .
 
