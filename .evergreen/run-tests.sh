53c87bef765272cd1608784beab1c8eb4e959fe6
Steven <steve.silvester@mongodb.com>
2024-06-12 08:59:23 -0500

    use pymongo test

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -7,20 +7,14 @@ set -eu
 bash ${DRIVERS_TOOLS}/.evergreen/secrets_handling/setup-secrets.sh drivers/enterprise_auth
 source secrets-export.sh
 
-# Map secrets
-export KERBEROS_SERVICE=${PRINCIPAL}
-export KERBEROS_PRINCIPAL=
-export KERBEROS_UPN=
-export KERBEROS_USER=${SASL_USER}
-export KERBEROS_DOMAIN=
-export KERBEROS_PASSWORD=${SASL_PASS}
-export KERBEROS_CANONICALIZE_HOSTNAME=1
-
 # Set up env
-"C:/python/Python38/python.exe" -m venv .venv
-.venv/Scripts/pip install pymongo pytest
-.venv/Scripts/pip install -e .
+git clone https://github.com/mongodb/mongo-python-driver
 
-# TODO: handle xunit results
-# Run tests
-.venv/Scripts/pytest
+"C:/python/Python38/python.exe" -m venv .venv
+dos2unix -q ".venv/Scripts/activate" || true
+.venv/Scripts/activate
+pip install "./mongo-python-driver[test]"
+pip install -e .
+
+export TEST_ENTERPRISE_AUTH=1
+bash ./mongo-python-driver/run-tests.sh
