fcd2ea78539946e0fe023f620dbdf80549532033
Steven <steve.silvester@mongodb.com>
2024-06-13 08:47:13 -0500

    fix shell script

--- .evergreen/assign-pr-reviewer.sh
+++ .evergreen/assign-pr-reviewer.sh
@@ -3,5 +3,6 @@
 set -x
 export CONFIG=$PROJECT_DIRECTORY/.github/reviewers.txt
 export SCRIPT="$DRIVERS_TOOLS/.evergreen/github_app/assign-reviewer.sh"
+# shellcheck disable=SC2154
 bash $SCRIPT -p $CONFIG -h ${github_commit} -o "mongodb" -n "winkerberos"
 echo '{"results": [{ "status": "PASS", "test_file": "Build", "log_raw": "Test completed"  } ]}' > ${PROJECT_DIRECTORY}/test-results.json
